e654e2d986be9f11651dcde25b6f6d10d48731a1
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-03-23 19:22:19 +0300

    finished it

--- Easy/sorted_squared.py
+++ Easy/sorted_squared.py
@@ -6,14 +6,28 @@ def sorted_squared(sorted_list):
 
 #smarter way of handling this situation is
 def sorting_linear_time(sorted_array):
-    new_array=[]
+    """the solutions should be arranged on incresing order from left to right
+    meaning that the smallest number is on the far left side while the biggest 
+    is on the far right.
+
+    Args:
+        sorted_array (list): this is order list(array)
+    """
+    new_array=[0 for _ in sorted_array]
     start=0
     end=len(sorted_array)-1
-    while True:
-        if abs(sorted_array[start]) > abs(sorted_array[end]):
-            new_array[]
+    for i in reversed(range(len(sorted_array))):
+        smallest=sorted_array[start]
+        biggest=sorted_array[end]
+        if abs(smallest)>abs(biggest):
+            new_array[i]=smallest**2
+            start+=1
+        else:
+            new_array[i]=biggest**2
+            end-=1
+    return new_array  
 
 #test mechanism
-lis=[-3,-2,1,4]
+lis=[-3,-2,10,20,40]
 print(f"\noriginal value is:{lis}")
-print(f" altered value is :{sorted_squared(lis)}")
+print(f" altered value is :{sorting_linear_time(lis)}")
---
+++ Easy/tournament.py
@@ -0,0 +1,7 @@
+# def tournament_winner(tournaments,scores):
+#     t_shape=len()12
+
+import numpy as np
+
+p=np.array(((12,34,34),(12,34,54)))
+print(p.shape)
