7cadcc4905041b3fe08f10c8395ae1ac669bd405
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-03-26 20:18:01 +0300

    made small progress

--- Easy/tournament.py
+++ Easy/tournament.py
@@ -1,7 +1,28 @@
-# def tournament_winner(tournaments,scores):
-#     t_shape=len()12
+def tournament_winner(compitition,scores):
+    """returns the team with the highest score
 
-import numpy as np
+    Args:
+        compitition (list): contains a lits where the names of the compition are listed
+        scores (_type_): contains the index of who won the compition of every round held
+    """
 
-p=np.array(((12,34,34),(12,34,54)))
-print(p.shape)
+   # best_score=""  #empty value
+    saved_scores=dict()
+    for idx,teams in enumerate(compitition):
+        index=scores[idx]  #index of the highest score
+        winner=teams[index] #the winner of the compitition 
+        if winner in saved_scores.keys():
+            saved_scores[winner]+=3
+        else:
+            saved_scores[winner]=3
+    return max(saved_scores)
+
+
+comp=[
+    ['a','b'],
+    ['c','a'],
+    ['b','f']
+]
+
+score=[0,0,1]
+print(tournament_winner(comp,score))
